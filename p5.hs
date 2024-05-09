75732b373b247ed7eedb297d2b2a5494ef5ae39b
AleR94 <88353061+AleR94@users.noreply.github.com>
2024-05-09 15:20:15 -0300

    Add files via upload

--- p5.hs
+++ p5.hs
@@ -1,178 +1,139 @@
+--Ejercicio 1
+--lista infinita de 1
+inf1 :: [Int]
+inf1 = 1 : inf1
 
---1) Generar una lista infinita de unos.
+--Ejercicio 2
+--Lista infinita a partir de un n dado
+infn :: Int ->[Int]
+infn n = n : infn (n+1)
 
-inf :: [Int]
-inf =  1 :  inf    
+--Ejercicio 3
+--Funcion que genera los primeros n naturales
 
-
---2). Generar una lista infinita de naturales comenzando desde un n ́umero dado.
 infNat :: Int -> [Int]
-infNat n = n : infNat(n+1)
+infNat 0 = [0] 
+infNat n = infNat (n-1) ++ [n]
 
---3) Generar una lista con los primeros n naturales
+--Ejercicio 4
+--Funcion que retorna los primeros 5 elementos de una lista inf de enteros positivos
+numeros :: [Int]
+numeros = enumFrom 0
 
-pNat :: Int -> [Int]
-pNat n 
-        | n == 0 = []
-        | otherwise = n : pNat(n-1)
+inf5 :: [Int] -> [Int]
+inf5 numeros = take 5 numeros
 
- --4)  Retornar los primeros 5 elementos de una lista infinita de enteros positivos.
+--Ejercicio 5
+--Dada una lista de enteros, retornar sus cuadrados
+cuad :: [Int] -> [Int]
+cuad [] = []
+cuad (x:xs) = map (^2) (x:xs) 
 
-intPos :: [Int] -> [Int]
-intPos  xs  = take 5 xs
---Todo en uno usando el enum
-intPos' :: Int->[Int] 
-intPos' n = take n[1..]
+--Ejercicio 6
+--Dado un entero, retornar sus divisores
 
-
---Generador de lista inf
-numInf :: [Int]
-numInf = enumFrom 0
-
---Utilizando funciones de alto orden resolver:
-
---5) Dada una lista de enteros, retornar sus cuadrados, es decir, dado [x0, x1, . . . , xn]
-
-retQ :: [Int]->[Int] 
-retQ []=[]
-retQ (x:xs) = (x*x):retQ(xs)
-
-mapCuadrado :: (Num a) => [a] -> [a]
-mapCuadrado [] = error "Empty List"
-mapCuadrado list = map (\x -> x*x) list
-
--- Ejemplo de uso
--- mapCuadrado [1, 2, 3, 4, 5] devuelve [1, 4, 9, 16, 25]
---6) dado un entero positivo, retornar una lista de sus divisores 
-retDiv :: Int->[Int]
-retDiv n = [x | x <- [1..n], n `mod` x == 0 ]   
+esDiv :: Int -> Int-> Bool
+esDiv n 0 = False
+esDiv 0 n = False
+esDiv n x= mod n x == 0 
 
 divisores :: Int -> [Int]
-divisores n = filter (\x ->  mod n x == 0) [1..n]
+divisores n = filter (esDiv n) [-n..n]
 
---7. Dada una lista de naturales, obtener la lista que contenga solo los n ́umeros
---primos de la lista original.
+--Ejercicio 7
+--dada una lista de naturales, obtener una lista de los naturales que son primos
+primos :: Int -> Int -> Bool
+primos x 1 = False
+primos x y = mod x y == 0 || primos x (y-1)
 
---primNum :: [Int]->[Int]
---primNum [a] = filter (\x -> x `mod` x == 0)
+prim2 :: Int -> Bool
+prim2 1 = False 
+prim2 2 = True
+prim2 x = not (primos x (x-1))
 
+primm :: [Int] -> [Int]
+primm xs = filter prim2 xs
 
---8 *. Dada una lista de naturales, retornar la suma de los cuadrados de la lista.
-retSum :: [Int]->Int
-retSum [] = error "empty list"
-retSum xs = foldr (\x acc-> acc + x*x) 0 xs 
+--Ejercicio 8 
+--Dada una lista de naturales, devolver la suma de los duadrados de la lista
+sumCuad :: [Int]-> Int
+sumCuad xs = sum (map (^2) xs)
 
+--Ejercicio 9
+--dada una lista de nat, retornar la lista de los sucesores
+succ :: [Int] -> [Int]
+succ [] = error "Lista Vacia"
+succ (x:xs) = map (+1) (x:xs)
 
---9. Dada una lista de naturales, retornar la lista con sus sucesores.
-retSucc :: [Int]->[Int] 
-retSucc xs = map (+1) xs 
+--Ejercicio 10
+--Dada una lista de enteros, sumar sus elementos
+suma :: [Int] -> Int
+suma [] = error "Lista vacia, no hay elementos para sumar"
+suma (x:xs) = foldl (+) 0 (x:xs)
 
---10. Dada una lista de enteros, sumar todos sus elementos.
+--Ejercicio 11
+--Hacer el factorial usando fold
+factorial :: Int -> Int
+factorial n = foldr (*) 1 [1..n]
 
-retSum1 :: [Int]->Int 
-retSum1 (x:xs) = foldl (\x acc-> acc + x) 0 (x:xs)
+--Ejercicio 12
+--redefinir el and usando fold
+andFold :: [Bool] -> Bool
+andFold xs = foldr (&&) True xs
 
--- 11 Definir el factorial usando fold.
-factFold :: Int->Int
-factFold x = foldl (*) 1 [1..x]
+--Ejercicio 13
+--funcion que devuelve la cantidad de elementos de una lista
+tam :: [a] -> Int
+tam [] = error "lista vacia"
+tam (x:xs) = foldl s 0 (x:xs)
 
+s :: Int -> a -> Int
+s x a = x+1
 
---12 *. Redefinir la funci ́on 'and' tal que 'and xs' se verifica si todos los elementos de xs son verdaderos. 
+--Ejercicio 14
+listaSuc :: [Int] -> [Int]
+listaSuc xs = [x+1 | x <- xs]
 
---Por ejemplo: and [1<2, 2<3, 1/=0] = True,
---and [1<2, 2<3, 1 == 0] = False.
+--Ejercicio 15
+listaCuad :: [Int] -> [Int]
+listaCuad xs = [ x * x | x <- xs]
 
-andxs :: [Bool]-> Bool 
-andxs [] = True
-andxs (x:xs) = x && andxs xs  
+--Ejercicio 16
+listaPar :: [Int] -> [Int]
+listaPar xs = [ x | x <- xs , even x, x > 10]
 
-andxs' :: [Bool] -> Bool 
-andxs' [] = True 
-andxs' xs = foldl (&&) True xs
+{--Ejercicio 17
+listaDiv :: [Int] -> [Int]
+listaDiv xs -}
 
+--Ejercicio 19
+numPrim :: Int -> [Int]
+numPrim n = [z | z <- [2..n], esPrimo z ] 
+esPrimo :: Int-> Bool
+esPrimo 0 = False
+esPrimo 1 = False
+esPrimo n = null [x | x <- [2..n-1], mod n x == 0]
 
+--Ejercicio 20
+cartesiano :: [a] -> [b] -> [(a,b)]
+cartesiano xs ys = [(x,y) | x <- xs, y <- ys]
 
---13. Usando foldl o foldr definir una funci ́on tam::[a]->Int que devuelve la
---cantidad de elementos de una lista dada. Dar un ejemplo en los cuales foldr y
---foldl evaluen diferente con los mismos parametros.
+--Ejercicio 21
+--dada una lista y un elemento retornar el numero de ocurrencias del elemento x en ys
+numberOcurrences :: Eq a => a -> [a] -> Int
+numberOcurrences x ys = length [z| z <- ys , z == x]
 
-tam :: [a] -> Int 
-tam [] = 0 
-tam (x:xs) = foldl (\acc _ -> acc + 1) 1 xs 
+--Ejercicio 22
+split2 :: [a] -> [([a],[a])]
+split2 xs = [(take a xs, drop a xs) | a <- [0..length xs]]
 
+--Ejercicio 23 (con el take obtengo los subsegmentnos iniciales de una lista)
+--dada una lista de enteros, devolver la suma de todos los subsegmentos
+sumaSegment :: [Int] -> Int
+sumaSegment xs = sum [sum (take a xs) | a <- [0..length xs]]
 
-
---Utilizando listas por comprensi ́on resolver:
-
---14. Dada una lista de enteros, retornar sus sucesores.
-
-retSucc' :: [Int]->[Int]
-retSucc' xs = [x + 1 | x <- xs] 
-
---15 *. Dada una lista de naturales, retornar sus cuadrados.
-retCuad :: [Int]->[Int]
-retCuad xs = [x*x | x<-xs]
-
---16. Dada una lista de enteros, retornar los elementos pares que sean mayores a 10.
-retPar :: [Int]->[Int]
-retPar xs = [x | x<-xs,even x, x>10]
-
-
-
---17. Dado un entero, retornar sus divisores.
-
-retDiv' :: Int->[Int] 
-retDiv' n = [x | x<-[1..n], n `mod` x == 0]
-
---18 *. Definir la funci ́on todosOcurrenEn :: Eq a => [a] -> [a] -> Bool tal que todosOcurrenEn xs ys se verifica si todos los elementos de xs son ele-
---mentos de ys. Por ejemplo: todosOcurrenEn [1,5,2,5] [5,1,2,4] = True, todosOcurrenEn [1,5,2,5] [5,2,4] = False
-
-sameElems :: Eq a => [a] -> [a] -> Bool 
-sameElems xs ys = and [elem x ys | x<-xs]
-
-sameElems' :: Eq a => [a] -> [a] -> Bool 
-sameElems' xs ys = and [x `elem` ys | x<-xs]
-
---19. Dado un natural n, retornar los n umeros primos comprendidos entre 2 y n.
-retPrim :: Int -> [Int]
-retPrim n = [x | x<-[2..n],esPrim x]
-
-esPrim :: Int -> Bool 
-esPrim n = n>1 && (length[x | x<-[2..n-1],n `mod` x == 0])==0
-
---20. Dadas dos listas de naturales, retornar su producto cartesiano.
-retCart :: [Int]->[Int]->[(Int,Int)]
-retCart xs ys = [(x,y) | x<-xs,y<-ys]
-
-retCart1 :: [a]->[b]->[(a,b)]
-retCart1 xs ys = [(x,y) | x<-xs , y<-ys]
---21 *. Dadas una lista y un elemento retornar el numero de ocurrencias del elemento x en la lista ys.
-retOcur :: Eq a => [a]->a->Int 
-retOcur xs n = length [x | x<-xs, x == n]
-
---22. Escribir la funci ́on split2 :: [a] - > [([a],[a])], que dada una lista xs, devuelve la lista con todas las formas de partir xs en dos. Por ejemplo:
---split2 [1,2,3] = [([],[1,2,3]), ([1],[2,3]), ([1,2],[3]),([1,2,3],[])].
-
-split2 :: [a]->[([a],[a])]
-split2 [] = [([],[])]
-split2 xs = [splitAt i xs | i<-[0..length xs]]
---23 *. Definir una funci ́on que, dada una lista de enteros, devuelva la suma de la suma de todos los segmentos iniciales.
---Por ejemplo: sumaSeg [1,2,3] = 0 + 1 + 3 + 6 = 10.
-
-sumaSeg :: [Int] -> Int
-sumaSeg xs = sum [sum (take i xs) | i <- [0..length xs]]
-
-
-
-
---24. Definir la lista infinita de los numeros pares.
-
---[x | x<-[0,2..]]
-
-infPares :: IO()
-infPares = mapM_ print [x | x<-[0,2..]] 
-
-infParesHasta :: Int -> [Int]
-infParesHasta n = [x | x<-[0,2..n]]
-
+--Ejercicio 24
+--lista infinita de numeros pares
+infiPar :: [Int] 
+infiPar = [x | x <- [0..], even x]
 
